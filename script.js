7de845703a656d7cef0c9d5e32027a2bbb23e0ad
Bright Kweku Amoah <bamoah@persol.com>
2024-04-09 15:05:38 +0000

    initial commit

--- script.js
+++ script.js
@@ -1,36 +1,109 @@
-"use strict";
+"use strict"
 
-const modal = document.querySelector(".modal");
-const overlay = document.querySelector(".overlay");
-const closeModalBtn = document.querySelector(".close-modal");
-const openModalBtns = document.querySelectorAll(".show-modal");
-// console.log(openModalBtns);
-// console.log(closeModalBtn);
-// console.log(overlay);
-// console.log(modal);
+// selecting elements
+const player0El = document.querySelector(".player--0");
+const player1El = document.querySelector(".player--1");
+const score0El = document.querySelector("#score--0");
+const score1El = document.getElementById("score--1");
+const current0El = document.getElementById("current--0");
+const current1El = document.getElementById("current--1");
+const diceEl = document.querySelector(".dice");
+const btnNew = document.querySelector(".btn--new");
+const btnRoll = document.querySelector(".btn--roll");
+const btnHold = document.querySelector(".btn--hold");
 
-const openModal = function () {
-    // console.log(`Buttton clicked`);
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
+let scores, currentScore, activePlayer, playing;
+
+const newGame = function () {
+
+    scores = [0, 0]
+    currentScore = 0;
+    activePlayer = 0;
+    playing = true;
+
+    score0El.textContent = 0;
+    score1El.textContent = 0;
+    current0El.textContent = 0;
+    current1El.textContent = 0;
+
+
+    diceEl.classList.add("hidden");
+    player0El.classList.remove("player--winner");
+    player1El.classList.remove("player--winner");
+    player0El.classList.add("player--active");
+    player1El.classList.remove("player--active");
+
+
+    console.log(scores);
+    console.log(`activeplayer:${activePlayer} currentScore: ${currentScore}`);
+}
+newGame();
+
+const switchPlayer = function () {
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    currentScore = 0;
+
+    console.log(`activeplayer:${activePlayer} currentScore: ${currentScore}`);
+
+    player0El.classList.toggle("player--active");
+    player1El.classList.toggle("player--active");
 };
-const closeModal = function () {
-    modal.classList.add('hidden');
-    overlay.classList.add('hidden');
-}
 
-for (let i = 0; i < openModalBtns.length; i++) {
-    openModalBtns[i].addEventListener("click", openModal);
-}
+//rolling dicefunctionality
+btnRoll.addEventListener("click", function () {
+    if (playing) {
+        //generate a random dice roll
+        const dice = Math.trunc(Math.random() * 6) + 1;
+        console.log(dice);
+        //display the dice roll
+        diceEl.classList.remove("hidden");
+        diceEl.src = `images/dice-${dice}.png`;
+        console.log(`dice: ${dice}`);
 
-closeModalBtn.addEventListener("click", closeModal);
+        //check for rolled 1: if true, switch to the next player
 
-overlay.addEventListener("click", closeModal);
+        if (dice !== 1) {
+            currentScore += dice;
 
-document.addEventListener("keydown", function (e) {
-    console.log(e.key);
-    if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+            document.getElementById(`current--${activePlayer}`).textContent = currentScore;
 
-        closeModal();
+            console.log(`activeplayer:${activePlayer} currentScore: ${currentScore}`);
+
+        } else {
+
+            switchPlayer();
+        }
     }
+
+
 });
+
+//hold button functionality
+btnHold.addEventListener('click', function () {
+    //add the current score to the scores array
+
+    if (playing) {
+        scores[activePlayer] += currentScore;
+        console.log(currentScore);
+        console.log(scores);
+
+        document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer];
+
+        if (scores[activePlayer] >= 100) {
+
+            playing = false;
+            diceEl.classList.add("hidden");
+
+            document.querySelector(`.player--${activePlayer}`).classList.add("player--winner");
+
+            document.querySelector(`.player--${activePlayer}`).classList.add("player--active");
+        } else {
+            switchPlayer();
+        }
+    }
+
+});
+
+//new game button functionality
+btnNew.addEventListener("click", newGame);
