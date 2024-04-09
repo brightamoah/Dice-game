0d87be8932ecd9ec21da27b1adf04073df891c0f
Bright Kweku Amoah <bamoah@persol.com>
2024-04-09 10:34:23 +0000

    modal in js

--- script.js
+++ script.js
@@ -1,82 +1,36 @@
 "use strict";
 
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const closeModalBtn = document.querySelector(".close-modal");
+const openModalBtns = document.querySelectorAll(".show-modal");
+// console.log(openModalBtns);
+// console.log(closeModalBtn);
+// console.log(overlay);
+// console.log(modal);
 
-let secretNumber = Math.trunc(Math.random() * 20) + 1;
-console.log(secretNumber);
+const openModal = function () {
+    // console.log(`Buttton clicked`);
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+};
+const closeModal = function () {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
 
-let score = 20;
-let highScore = 0;
+for (let i = 0; i < openModalBtns.length; i++) {
+    openModalBtns[i].addEventListener("click", openModal);
+}
 
-console.log(highScore, typeof highScore);
+closeModalBtn.addEventListener("click", closeModal);
 
-document.querySelector(".check").addEventListener("click", function () {
+overlay.addEventListener("click", closeModal);
 
-    const guess = Number(document.querySelector(".guess").value);
+document.addEventListener("keydown", function (e) {
+    console.log(e.key);
+    if (e.key === "Escape" && !modal.classList.contains("hidden")) {
 
-    console.log(guess, typeof guess);
-
-    if (!guess) {
-        document.querySelector(".message").textContent = "⛔ No number";
-    }
-    else if (guess === secretNumber) {
-        document.querySelector(".message").textContent = "🎉 Correct Number!";
-
-        document.querySelector(".number").textContent = secretNumber;
-
-        document.querySelector("body").style.backgroundColor = "#60b347";
-
-        document.querySelector(".number").style.width = "30rem";
-
-
-        if (score > highScore) {
-            highScore = score;
-            document.querySelector(".highscore").textContent = highScore;
-        }
-
-    } else if (guess > secretNumber) {
-        if (score > 1) {
-            document.querySelector(".message").textContent = "📈 Too high";
-            score -= 1;
-            document.querySelector(".score").textContent = score;
-        } else {
-            document.querySelector(".message").textContent = "😭 You Loose!!";
-
-            document.querySelector("body").style.backgroundColor = "#880808";
-        }
-    } else if (guess < secretNumber) {
-        if (score > 1) {
-            document.querySelector(".message").textContent = "📉 Too low";
-
-            score -= 1;
-            document.querySelector(".score").textContent = score;
-        } else {
-            document.querySelector(".message").textContent = "😭 You Loose!!";
-            document.querySelector("body").style.backgroundColor = "#880808";
-        }
+        closeModal();
     }
 });
-
-document.querySelector(".again").addEventListener("click", function () {
-
-    secretNumber = Math.trunc(Math.random() * 20) + 1;
-    console.log("secret Number:", secretNumber);
-
-    score = 20;
-
-    document.querySelector(".message").textContent = "Start guessing ....";
-
-    document.querySelector("body").style.backgroundColor = "#222";
-
-    document.querySelector(".number").style.width = "15rem";
-
-    document.querySelector(".score").textContent = score;
-
-    document.querySelector(".number").textContent = "?";
-
-    document.querySelector(".guess").value = '';
-
-    console.log("high Score:", document.querySelector(".highscore").textContent = highScore,);
-
-});
-
-
